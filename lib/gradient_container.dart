cf520ab493ea788094447965fc564ef03349d2e7
nitishgupta9785 <nitishgupta9785@gmail.com>
2023-06-13 22:34:43 +0530

    added: Resuable widgets and Constructor Function

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -5,19 +5,25 @@ const startAlignment = Alignment.topRight;
 const endAlignment = Alignment.bottomLeft;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({
+  const GradientContainer(
+    this.color1,
+    this.color2,
+    this.color3, {
     super.key,
   });
+
+  final Color color1;
+  final Color color2;
+  final Color color3;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Color.fromARGB(255, 15, 6, 78),
-          Color.fromARGB(255, 26, 10, 64),
-          Color.fromARGB(255, 13, 2, 38)
-        ], begin: startAlignment, end: endAlignment),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: [color1, color2, color3],
+            begin: startAlignment,
+            end: endAlignment),
       ),
       child: const Center(child: StyleText('Nitish gupta')),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,8 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: GradientContainer(Color.fromARGB(255, 15, 6, 78),
+            Color.fromARGB(255, 26, 10, 64), Color.fromARGB(255, 13, 2, 38)),
       ),
     ),
   );
